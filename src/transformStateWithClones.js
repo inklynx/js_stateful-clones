97a00c04588f5954aaa30fc9e5f0c6a3f1d5bc7c
SquareSquirrell <zettahedron@gmail.com>
2026-06-07 14:56:54 +0200

    add fixes after AI-mentor review

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -29,6 +29,9 @@ function transformStateWithClones(state, actions) {
           delete newState[key];
         }
         break;
+
+      default:
+        throw new Error(`Unsupported action.type: ${action.type}`);
     }
 
     result.push(newState);
